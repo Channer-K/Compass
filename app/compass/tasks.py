47a56452400e482f42d0e682cf3334ac95ad88ff
Channer-K <Channer@live.cn>
2014-11-04 12:44:17 +0800

    11-04-02

--- app/compass/tasks.py
+++ app/compass/tasks.py
@@ -4,42 +4,40 @@ from __future__ import absolute_import
 import datetime
 from app.celery import app
 from compass.conf import settings
-from django.conf import settings as djsettings
 from compass.utils.notification import send_email
 
 
 @app.task
 def automatic_dist():
-    from django.db.models import Q
-    from compass.models import Subtask
-    w_subtasks = Subtask.objects.filter(
-        Q(editable=True),
-        Q(assignee__isnull=True),
-        Q(status_id=settings.WaitingForPost_Status)
-    )
+    from compass.models import Task, Subtask
+    from compass.utils.helper import get_right_assignee
 
-    if w_subtasks:
-        from compass.utils.helper import get_right_assignee
-        for st in w_subtasks:
-            delta = datetime.datetime.now()-st.updated_at
+    tasks = Task.objects.filter(editable=True, available=True)
 
-            if (delta.total_seconds() / djsettings.SCHEDULE_PERIOD) >= 1:
-                pub_date = datetime.datetime.now()+datetime.timedelta(hours=2)
+    for task in tasks:
+        progress_id = task.progress_id
+        subtask = Subtask.objects.get(pk=progress_id)
 
-                st.assignee = get_right_assignee()
-                st.pub_date = pub_date
-                st.status = st.get_next_status()
+        delta = datetime.datetime.now() - subtask.updated_at
 
-                st.save(update_fields=['assignee', 'pub_date', 'status'])
+        if (delta.total_seconds() / settings.SCHEDULE_PERIOD) >= 1:
+            pub_date = datetime.datetime.now() + datetime.timedelta(hours=2)
 
-                subject = u'【新任务】'+"("+st.status.name+")"+st.task.amendment
-                contxt = {'applicant': st.task.applicant,
-                          'at_time': st.task.created_at,
-                          'task_title': st.task.amendment,
-                          'version': st.task.version
-                          }
+            subtask.assignee = get_right_assignee()
+            subtask.pub_date = pub_date
+            subtask.status = subtask.get_next_status()
 
-                send_email.delay(subject=subject,
-                                 to=[st.assignee.email],
-                                 template_name='schedule',
-                                 extra_context=contxt)
+            subtask.save(update_fields=['assignee', 'pub_date', 'status'])
+
+            subject = u'【新任务】' + "(" + subtask.status.name + ")" + \
+                subtask.task.amendment
+            contxt = {'applicant': subtask.task.applicant,
+                      'at_time': subtask.task.created_at,
+                      'task_title': subtask.task.amendment,
+                      'version': subtask.task.version
+                      }
+
+            send_email.delay(subject=subject,
+                             to=[subtask.assignee.email],
+                             template_name='schedule',
+                             extra_context=contxt)
